24da2139c6a98b6f036673576034644812579060
Tharun Guda <tharunguda7@gmail.com>
2025-03-26 20:51:54 -0500

    DONE2

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Guda Tharun' });
+  res.render('mydata', { title: 'Guda Tharun' });
 });
 
 module.exports = router;
